398e46ce83716eace97a0e4dff46ffc4285d79d5
ahtarv <atharvkpatil@gmail.com>
2026-02-13 09:38:05 +0530

    added the logic from B from contest 1079

--- Problem_B_Contest_1079.java
+++ Problem_B_Contest_1079.java
@@ -20,8 +20,45 @@ public class Problem_B_Contest_1079 {
             for (int j = 0; j < length; j++) {
                 secarr[j] = sc.nextInt();
             }
+            int[] pos = new int[length + 1];
+for (int i = 0; i < length; i++) {
+    pos[arr[i]] = i;
+}
 
-            // logic goes here
+int[] left = new int[length + 1];
+int[] right = new int[length + 1];
+
+Arrays.fill(left, length);
+Arrays.fill(right, -1);
+
+for (int i = 0; i < length; i++) {
+    int v = secarr[i];
+    left[v] = Math.min(left[v], i);
+    right[v] = Math.max(right[v], i);
+}
+
+boolean ok = true;
+
+for (int x = 1; x <= length; x++) {
+    if (right[x] == -1) continue;
+
+    if (pos[x] < left[x] || pos[x] > right[x]) {
+        ok = false;
+        break;
+    }
+
+    for (int i = left[x]; i <= right[x]; i++) {
+        if (secarr[i] != x) {
+            ok = false;
+            break;
+        }
+    }
+
+    if (!ok) break;
+}
+
+System.out.println(ok ? "YES" : "NO");
+
 
         }
 
